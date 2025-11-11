40e4f86cdff50d6d49a1970bc0c8cdb44943626b
AndreiPavlov1988 <ai_pavlov@mail.ru>
2025-11-11 12:25:59 +0300

    task21

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        int [] array = new int [3];
-        array [0] = 1;
-        array [1] = 2;
-        array [2] = 3;
-        double [] array1 = {1.57,7.654,9.986};
+        int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        double[] array1 = {1.57, 7.654, 9.986};
         String[] array2 = {"красный", "зеленый", "синий", "желтый"};
-    //task2
-    int [] array3 = {2,3,7,8};
-    System.out.println(Arrays.toString (array3));
+        //task2
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1)
+                System.out.print(", ");
+        }
+        System.out.println();
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i]);
+            if (i < array1.length - 1)
+                System.out.print(", ");
+        }
+        System.out.println();
+        for (int i = 0; i < array2.length; i++) {
+            System.out.print(array2[i]);
+            if (i < array2.length - 1)
+                System.out.print(", ");
+        }
+        //task3
     }
-    }
+}
