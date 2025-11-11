01d576b510abac5916e98fb2448800b3adc4f054
AndreiPavlov1988 <ai_pavlov@mail.ru>
2025-11-11 11:32:21 +0300

    task1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] array = new int [3];
+        array [0] = 1;
+        array [1] = 2;
+        array [2] = 3;
+        double [] array1 = {1.57,7.654,9.986};
+        String[] array2 = {"красный", "зеленый", "синий", "желтый"};
+        }
+    }
